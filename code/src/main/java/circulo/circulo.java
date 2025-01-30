75c0a814823eec6a5bb05861ef5b738579fbfb0a
Sergio Campos <campos.delgado.sergio@iescamas.es>
2025-01-30 11:03:43 +0100

    no hice nada

---
+++ code/src/main/java/circulo/circulo.java
@@ -0,0 +1,22 @@
+package circulo;
+
+public class circulo extends com.endes.figuras.FiguraGeometrica{
+
+	public circulo(String nombre, String color) {
+		super(nombre, color);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double area() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double perimetro() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
