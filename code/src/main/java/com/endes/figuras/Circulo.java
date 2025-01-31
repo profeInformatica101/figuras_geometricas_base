cd7a9bd20c864351c4d7fc5b9357f67db6e65460
Juan Carlos Pizarro Alonso <127835047+juancarlospizarro@users.noreply.github.com>
2025-01-31 16:45:30 +0100

    Finalizada la clase Circulo a falta de las pruebas unitarias.

--- code/src/main/java/com/endes/figuras/Circulo.java
+++ code/src/main/java/com/endes/figuras/Circulo.java
@@ -1,72 +1,79 @@
 package com.endes.figuras;
 
-/*
+/**
+ * Clase que representa un circulo
+ * Hereda de la clase FiguraGeometrica
+ * 
  * @author Juan Carlos Pizarro Alonso
  */
-
 public class Circulo extends FiguraGeometrica{
 	
-	//private Punto centro;
+	private Punto centro;
 	private double radio;
+	private Punto inicio;
 
-	/*
-	 * @param
+	/**
+	 * Constructor de la clase Circulo
 	 * 
-	 * @return
+	 * @param nombre Nombre del circulo
+	 * @param color Color del circulo
+	 * @param centro Punto que representa el centro del circulo
+	 * @param radio Radio del circulo
+	 * 
 	 */
-	
-	// falta Punto centro
-	
-	public Circulo(String nombre, String color, double radio) {
+	public Circulo(String nombre, String color, Punto centro, double radio) {
 		super(nombre, color);
+		this.centro = centro;
 		this.radio = radio;		
 	}
 
-	/*
-	 * @param
-	 * 
-	 * @return
+	/**
+	 * Metodo para obtener el area del circulo
+	 *  
+	 * @return Area del circulo
 	 */
-	
 	@Override
 	public double area() {
-		// TODO Auto-generated method stub
 		return Math.PI * (radio * radio);
 	}
 
-	/*
-	 * @param
+	/**
+	 * Metodo para obtener el perimetro del circulo
 	 * 
-	 * @return
-	 * 
+	 * @return Perimetro del circulo
 	 */
-	
 	@Override
 	public double perimetro() {
-		// TODO Auto-generated method stub
 		return 2 * Math.PI * radio;
 	}
 	
-	/*
-	 * @param
+	/**
+	 * Metodo para rotar el circulo el angulo indicado
 	 * 
-	 * @return
+	 * @param angulo Angulo que rota el circulo
+	 * 
 	 */
-	
+	@Override
 	public void rotar(double angulo) {
-		
+		double X1 = centro.getX() + radio;
+		inicio = new Punto(X1, centro.getY());
+		double X2 = centro.getX() + (X1 - centro.getX()) * Math.cos(angulo) - (centro.getY() - centro.getY()) * Math.sin(angulo);
+		double Y2 = centro.getY() + (X1 - centro.getX()) * Math.sin(angulo) + (centro.getY() - centro.getY()) * Math.cos(angulo);
+		inicio.setX(X2);
+		inicio.setY(Y2);
 	}
 	
-	/*
-	 * @param
+	/**
+	 * Metodo para trasladar el circulo a la posicion indicada
 	 * 
-	 * @return
+	 * @param dx Nueva posicion del eje X del circulo
+	 * @param dy Nueva posicion del eje Y del circulo
+	 * 
 	 */
-	
+	@Override
 	public void trasladar(double dx, double dy) {
-		
+		centro.setX(dx);
+		centro.setY(dy);
 	}
-
-	
 
 }
