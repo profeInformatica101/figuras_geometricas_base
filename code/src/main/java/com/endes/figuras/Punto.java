120ce56ed5134ac5f5284e66a9c2343364d7d1a7
OEM Configuration (temporary user) <rios.garcia.marcos@iescamas.es>
2025-01-31 10:24:28 +0100

    clase punto finalizada con todos sus metodos y constructor

--- code/src/main/java/com/endes/figuras/Punto.java
+++ code/src/main/java/com/endes/figuras/Punto.java
@@ -1,5 +1,34 @@
 package com.endes.figuras;
 
 public class Punto {
-
+	private double x;
+	private double y;
+	
+	public Punto(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	public void setX(double x) {
+		this.x = x;
+	}
+	
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	public double distancia(Punto otro) {
+		double dx = (otro.getX() - x) * (otro.getX() - x);
+		double dy = (otro.getY() - y) * (otro.getY() - y);
+		double res = Math.sqrt(dx + dy);
+		return res;
+	}
 }
