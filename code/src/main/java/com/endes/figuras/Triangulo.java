934e2e848a51bc553f8ce570174cedf3063bc69a
Alejandro <hernandez.razo.alejandro@iescamas.es>
2025-01-31 11:12:16 +0100

    Completada la clase triangulo y pruebas test

--- code/src/main/java/com/endes/figuras/Triangulo.java
+++ code/src/main/java/com/endes/figuras/Triangulo.java
@@ -4,24 +4,32 @@ public class Triangulo extends FiguraGeometrica {
 	private Punto = vertice1; //representa la base
 	private Punto = vertice2;	// representamos altura
 	private Punto = vertice3;
+	
 	public Triangulo(String nombre, String color, Punto v1, Punto v2, Punto v3) {
 		super(nombre,color);
-	}
-	
-
-
-
-	@Override
-	public double area(Punto v1, Punto v2) {
-		double resultado;
-		resultado= (v1*v2)/2
-		return resultado;
-	}
-
-	@Override
-	public double perimetro(Punto v1, Punto v2, Punto v3) {
-		double res=v1+v2+v3;
 		
-		return res;
 	}
+
+	@Override
+	public double area() {
+		
+		return (v1*v2)/2;
+	}
+
+	@Override
+	public double perimetro() {
+		
+		return v1+v2+v3;
+	}
+
+	
+	public void rotar(double angulo) {
+		
+	}
+	public void trasladar(double dx, double dy) {
+        
+    }
+
+
+
 }
---
+++ code/src/test/java/testtriangulo.java
@@ -0,0 +1,14 @@
+package com.endes.figuras;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testtriangulo {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
