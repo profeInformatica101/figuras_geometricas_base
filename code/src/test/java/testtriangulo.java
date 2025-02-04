3622f92fa45c60d2c9901ddc583880d8cbf71f5f
Alejandro <hernandez.razo.alejandro@iescamas.es>
2025-02-04 12:01:30 +0100

    cambios

--- code/src/test/java/testtriangulo.java
+++ code/src/test/java/testtriangulo.java
@@ -2,13 +2,27 @@ package com.endes.figuras;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.endes.instrumento.Termometro;
 
 class testtriangulo {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	//private Triangulo triangulo;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		System.out.println("Antes de todas las pruebas");
 	}
-
+	@BeforeEach
+	void setUp() throws Exception{
+		termometro = new Triangulo("Equilatero", "negro", 1, 1, 1);
+	}
+	@test
+	@Displayname (Area del triangulo)
+	public void calculaArea();
+		double resultadoEsperado=0;
+		triangulo.
 }
